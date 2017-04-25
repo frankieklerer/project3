58767ac809413e1981f99dff3ff9501d6056c66c
frankieklerer <frankie@klerer.net>
2017-04-25 10:06:28 -0400

    hi

--- router.java
+++ router.java
@@ -6,9 +6,18 @@ public class router {
 	private int ipAddress;
 	private int portNumber;
 	private int poisonedReverse;
+	private ArrayList<ArrayList<String>> currentNodeList;
 
 	public static void main(String[] args){
-
+		
+		
+		if(args.length<2){
+            System.out.println("need at least 2 arguements");
+            System.exit(1);
+        }
+        poisonedReverse = Integer.parseInt(args[0]);
+        currentNodeList = readFile(args[1]);
+        
 	}
 
 	public router()
@@ -19,10 +28,10 @@ public class router {
  	
 
  	
-    public static ArrayList<ArrayList<Integer>> readFile(String fileName){
+    public static ArrayList<ArrayList<String>> readFile(String fileName){
 
-        ArrayList<ArrayList<Integer>> nodeArray = new ArrayList<ArrayList<Integer>>();
-        int count = 0;
+        ArrayList<ArrayList<String>> nodeArray = new ArrayList<ArrayList<String>>();
+        boolean first = true;
         Scanner scan = null;
 
 
@@ -33,14 +42,29 @@ public class router {
 
         while(scan.hasNextLine())
         {
-        	if(count == 0)
+        	if(first)
         	{
-        		count++;
+        		String strLine = scan.nextLine();
+            	String[] st = strLine.split(" ");
+            	ipAddress = st[0];
+            	portNumber = st[1];
+        		first = false;
         		continue;
         	}
+        	
             String strLine = scan.nextLine();
+
             String[] s = strLine.split(" ");
-            
+            String tempIP = s[0];
+            String tempPort = s[1];
+            String tempCost = s[2];
+
+            ArrayList<String> tempNodeInfo = new ArrayList<String>();
+            tempNodeInfo.add(tempIP);
+            tempNodeInfo.add(tempPort);
+            tempNodeInfo.add(tempCost);
+
+            nodeArray.add(tempNodeInfo);
         }
 
 
