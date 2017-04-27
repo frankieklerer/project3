3cf8bfd5bf547d783d6124aee6eef8fcd3f955bc
Agathe Benichou <agatha.benichou@gmail.com>
2017-04-27 08:28:03 -0400

    edits to router class

--- router.java
+++ router.java
@@ -1,72 +1,102 @@
 import java.util.*;
 import java.io.FileReader;
 
+/**
+* The router class responsibe for updating 
+**/
 public class router {
 
-	private int ipAddress;
-	private int portNumber;
-	private int poisonedReverse;
-	private ArrayList<ArrayList<String>> currentNodeList;
+    // every router has a globally unique IP address
+	private static int ipAddress;
+
+    // every router has a globally unique port number
+	private static int portNumber;
+
+    // every router need to know whether they can use Poisoned Reverse or not
+    // if 0 , router does not use PR. if 1, router does use PR.
+	private static int poisonedReverse;
+
+    // an Array List where each index stores an Array List which contains IP address, port number and direct cost of known routers
+	private static ArrayList<ArrayList<String>> forwardingTable;
 
 	public static void main(String[] args){
-		
-		
+		 
 		if(args.length<2){
-            System.out.println("need at least 2 arguements");
+            System.out.println("The parameter needs 2 arguments: whether the router uses Poisioned Reverse or not (a 0 or a 1) and the text file which specifies the routers direct neighbors and the cost");
             System.exit(1);
         }
-        poisonedReverse = Integer.parseInt(args[0]);
-        currentNodeList = readFile(args[1]);
+
+        router newRouter = new router(args);
         
 	}
 
-	public router()
-	{
+	public router(String[] args){
+
+        // assigning the first argument to global variable
+        this.poisonedReverse = Integer.parseInt(args[0]);
+
+        // initializing the global array list from the method
+        this.forwardingTable = this.readFile(args[1]);
+
+        System.out.println(this.forwardingTable);
 
 	}
 
  	
 
- 	
-    public static ArrayList<ArrayList<String>> readFile(String fileName){
+ 	/**
+    * Method that reads in the neighbors.txt file for each router
+    **/
+    public ArrayList<ArrayList<String>> readFile(String fileName){
 
+        // temporary array list
         ArrayList<ArrayList<String>> nodeArray = new ArrayList<ArrayList<String>>();
-        boolean first = true;
+
+        // tells the reader if its the first nline of the text file or not
+        boolean firstLine = true;
         Scanner scan = null;
 
-
         try{
+
             scan = new Scanner(new FileReader(fileName));
+
+            while(scan.hasNextLine()){
+            
+                // if it is the first line of the text file
+                if(firstLine){
+
+                    String firstStr = scan.nextLine();
+                    String[] myInfo = firstStr.split(" ");
+                    this.ipAddress = Integer.parseInt(myInfo[0]);
+                    this.portNumber = Integer.parseInt(myInfo[1]);
+
+                    // already read first line
+                    firstLine = false;
+                    continue;
+                }
+            
+                // all of its neighbors
+                String finalStr = scan.nextLine();
+
+                // extract node info
+                String[] myNeighbors = finalStr.split(" ");
+                String tempIP = myNeighbors[0];
+                String tempPort = myNeighbors[1];
+                String tempCost = myNeighbors[2];
+
+                // add to an array list
+                ArrayList<String> tempRouterInfo = new ArrayList<String>();
+                tempRouterInfo.add(tempIP);
+                tempRouterInfo.add(tempPort);
+                tempRouterInfo.add(tempCost);
+
+                // add arraylist to bigger arraylist
+                nodeArray.add(tempRouterInfo);
+            }
+
         }catch(Exception e){
-            System.out.println("Could not open file " + e);}
-
-        while(scan.hasNextLine())
-        {
-        	if(first)
-        	{
-        		String strLine = scan.nextLine();
-            	String[] st = strLine.split(" ");
-            	ipAddress = st[0];
-            	portNumber = st[1];
-        		first = false;
-        		continue;
-        	}
-        	
-            String strLine = scan.nextLine();
-
-            String[] s = strLine.split(" ");
-            String tempIP = s[0];
-            String tempPort = s[1];
-            String tempCost = s[2];
-
-            ArrayList<String> tempNodeInfo = new ArrayList<String>();
-            tempNodeInfo.add(tempIP);
-            tempNodeInfo.add(tempPort);
-            tempNodeInfo.add(tempCost);
-
-            nodeArray.add(tempNodeInfo);
+            System.out.println("Could not open file " + e);
         }
-
 
         return nodeArray;
     }
